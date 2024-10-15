dc358f84594770cd2e1e5279867d5051a9e219a9
Lubwama Collins <bizzicole87@example.com>
2024-10-15 16:14:45 +0300

    Fix: navbar fixed

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,7 +4,7 @@ import { SVGProps } from "react"
 export default function Navbar() {
     return (
         <header className="flex items-center h-16 px-4 border-b bg-white shadow-sm sticky">
-        <Link href="/" className="text-2xl font-bold text-red-600">REAGAN & GOOD FRIENDS CHARITY</Link>
+        <Link href="/" className="text-lg sm:text-xl md:text-2xl font-bold text-red-600">REAGAN & GOOD FRIENDS CHARITY</Link>
         <nav className="ml-auto font-medium">
           <Link className="mx-2 text-gray-500 hover:text-gray-900" href="#about">
             About
